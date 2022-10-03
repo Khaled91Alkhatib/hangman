37e3094ae3d937d479e411a85dc3b9070f9416c4
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-10-03 17:21:58 -0400

    Import components and set states and pass down components in return

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import React, { useState } from 'react';
+
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+import Popup from './components/Popup';
+import Notification from './components/Notification';
+import "./styles/App.scss";
+
+const words = ['application', 'universe', 'lot', 'development', 'evolution', 'basic', 'flight', 'vehicle'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
   return (
     <div>
-      Hangman
+      <Header />
+      <div className='game-container'>
+        <Figure />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
     </div>
   );
 }
