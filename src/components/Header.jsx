1c24fd7f99c437df5628a9a63018cb2e4f86020d
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-10-03 17:23:41 -0400

    Add components for the title of the game

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Hangman</h1>
+      <div>Find the hidden word.</div>
+    </div>
+  );
+};
+
+export default Header;
